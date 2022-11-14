9777b0d5b6ce417550b93ad89c5121e0ea451689
Ilya Siganov <ilya.blan4@gmail.com>
2022-11-14 14:27:01 +0600

    add roguelike aka turtle template

---
+++ tasks/task_07_roguelike/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <list>
+#include <string>
+#include <vector>
+
+void start(const std::vector<int>& field, std::size_t n, std::size_t m) {
+  // TODO: напиши меня!
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  std::size_t n, m;
+  std::cin >> n >> m;
+
+  std::vector<int> field;
+  for (std::size_t i = 0; i < n; i++) {
+    for (std::size_t j = 0; j < m; j++) {
+      std::cin >> field.at(i * m + j);
+    }
+  }
+
+  start(field, n, m);
+
+  return 0;
+}
